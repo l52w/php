3ace03022cdd9b5401c56b6ef401d79b060356b0
l52w <62008219+l52w@users.noreply.github.com>
2022-08-24 14:16:19 +0900

    Update array.php

--- array.php
+++ array.php
@@ -3,3 +3,10 @@ $cars = array("Volvo", "BMW", "Toyota");
 echo count($cars);
 echo $cars[2];
 ?>
+
+<br>
+
+<?php
+$books = array("소설", "수필", "만화", "과학");
+echo $books[0].", ".$books[1].", ".$books[2].", ".$books[3];
+?>
